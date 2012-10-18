ea67acbfe1ca48d98dd523d42a740ab41889eb5f
Ville Saalo <ZeroOne@iki.fi>
2012-10-18 21:48:47 +0300

    parse_row function, Question class; random stuff
    Created an initial version of the parse_row function, expanded the
    Candidate class, created an initial version of the Question class.
    
    Note that at the moment the source data contains some extra columns that
    actually mess up the parse_row function somewhere in the middle.

--- core.py
+++ core.py
@@ -13,8 +13,14 @@ CSV_DELIMITER = ','
 CSV_QUOTECHAR = '"'
 
 class Question(object):
-	def __init__(self):
-		pass
+	def __init__(self, question, answer, weight, comment):
+		self.question = question
+		self.answer = answer
+		self.weight = weight
+		self.comment = comment
+	
+	def __str__(self):
+		return "Question: %s\nAnswer: %s\nWeight: %s\nComment: %s" % (self.question, self.answer, self.weight, self.comment)
 
 class Municipality(object):
 	def __init__(self, name):
@@ -28,14 +34,18 @@ class Party(object):
 		self.name = name
 
 	def __str__(self):
-		return "%s" % self.
+		return "%s" % self.name
 
 class Candidate(object):
-	def __init__(self, lastname, firstname, party, municipality):
+	def __init__(self, lastname, firstname, party, municipality, age, www, facebook, twitter):
 		self.lastname = lastname
 		self.firstname = firstname
 		self.party = party # TODO: ref Party
 		self.municipality = municipality # TODO: ref Municipality
+		self.age = age
+		self.www = www
+		self.twitter = twitter
+		self.facebook = facebook
 
 	def __str__(self):
 		return "%s, %s (%s) / %s" % (self.lastname, self.firstname, self.party, self.municipality)
@@ -51,14 +61,27 @@ def read_data(filename, quotechar=CSV_QUOTECHAR, delimiter=CSV_DELIMITER):
 		data.append(row)
 	return data
 
-def parse_row(d):
-	raise NotImplementedError
+def parse_row(row):
+	candidate = Candidate(row.pop(0), row.pop(0), row.pop(0), row.pop(0), row.pop(0), row.pop(0), row.pop(0), row.pop(0));
+	questions = []
+	
+	for i in range(0, len(row)-3, 4):
+		question = Question(row[i+0], row[i+1], row[i+2], row[i+3])
+		questions.append(question)
+		
+	return candidate, questions
 
 def main(args):
 	# Read full CSV to list
 	everything = read_data(args.infile)
 
-	print everything[0:5]
+	# Pick the second candidate in the list and print her information:
+	parsed_row = parse_row(everything[2]);
+	candidate = parsed_row[0]
+	questions = parsed_row[1]
+	print candidate
+	for	question in questions:
+		print question, "\n"
 
 	# TODO: My answers = abcdefg, compare to data, calculate best party match
 
